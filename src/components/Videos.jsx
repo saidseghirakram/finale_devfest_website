c4140321f3c5cfb0e033583e48853faec834f177
momo56710 <mohamedmouh567@gmail.com>
2023-11-29 21:17:29 +0000

    fixing video width

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -8,7 +8,7 @@ const Videos = () => {
         <Fade key={nav.id} direction="up" delay={index * 150} triggerOnce>
           <div className="sm:w-[450px]  w-full flex flex-col">
            
-            <iframe className="h-[291px] self-stretch rounded-[30px] mb-[25px] object-cover" width="500" height="315" src={nav.URL} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe className="h-[291px] self-stretch rounded-[30px] w-[500px] max-sm:w-[400px] mb-[25px] object-cover"  height="315" src={nav.URL} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
             <h1 className="text-gray-700 font-inter text-lg font-bold leading-[140.523%] text-center">
               {nav.title}
