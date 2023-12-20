8a36770061a889e58e99766d8fb6936e60882d8e
S_AKRAM <akramsaidseghir26@gmail.com>
2023-12-20 19:44:14 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
             <Nav />
             <Hero />
           <About />
-          {/* <Agenda /> */}
+          <Agenda />
           <Speakers />
           <Registering />
           <Footer />
